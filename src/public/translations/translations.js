bc3b2ba6cf33c8910c0e517cecda30298bac9644
Lucas <rlucas92@gmail.com>
2023-01-12 23:26:32 +0000

    Translations

--- src/public/translations/translations.js
+++ src/public/translations/translations.js
@@ -8,7 +8,7 @@ const translations = {
       theWeddingTitle: "The Wedding",
       theWeddingLocation: "In Seville, Spain",
       theWeddingVenue: "At Villa Luisa.",
-      theWeddingDate: "Sunday, May 19th, 2024",
+      theWeddingDate: "Sunday, June 2nd, 2024",
       theWeddingDateComment: "The date is CONFIRMED.",
       travelSectionTitle: "Travel",
       travelSectionDescription: "<p align=\"left\"><strong>The wedding will be in Spain. It's important that you arrange your transportation and accommodation as soon as possible to avoid prices going up.</strong></p> \
@@ -20,7 +20,7 @@ const translations = {
                                  <br/> <span>&#8226;</span> By Taxi: There is a taxi rank next to the <a href=\"https://goo.gl/maps/4C1xuXBqvYmbxac37\">Cathedral</a>, and another next to the <a href=\"https://goo.gl/maps/92EYsPtYXFvyz6VY8\">Puerta de Jerez</a>. If you are staying in the city centre, those are probably the closest to you. If you give the driver the address (Avenida Manuel Siurot, 1), they'll know where it is. They take Euros or credit/debit card payments. A trip from the city centre should be around €6-8 and take 8 minutes. \
                                  <br/> <span>&#8226;</span> By Shuttle: to be confirmed.</p>\
                                  <h4 align=\"left\" style=\"padding-top:10px;\">Choosing accommodation</h4>\
-                                 <p align=\"left\">We recommend that you stay at a hotel or apartment within the city centre. Don't worry about being close to the venue, as you will want to be closer to the city centre most other days. Some recommendations, from cheapest to dearest: \
+                                 <p align=\"left\">We recommend that you stay at a hotel or apartment within the city centre. Don't worry about being close to the venue, as you will want to be closer to the city centre most other days. Some recommendations: \
                                  <br/> <span>&#8226;</span> <a href=\"https://www.booking.com/Share-ZC3yHL\">La Abadia de Giralda</a>: next to the cathedral, around £100 per night and double room.\
                                  <br/> <span>&#8226;</span> <a href=\"https://www.welldonehotels.com/nuestros-hoteles/\">One of the WellDone hotels or apartments</a>: They can be cheap or expensive. All the locations are very central.\
                                  <br/> <span>&#8226;</span> <a href=\"https://www.casasdeelarenal.com/en/\">Las Casas de El Arenal</a>: charming hotel, around £120 per night and double room.\
@@ -28,25 +28,25 @@ const translations = {
                                  <br/> <span>&#8226;</span> <a href=\"https://www.booking.com/hotel/es/casa-1800.en-gb.html\">Hotel Casa 1800</a>: beautiful hotel with rooftop pool and views of the cathedral, £140.\
                                  <br/> <span>&#8226;</span> <a href=\"https://taykohotels.com/en/sevilla/habitaciones/\">Hotel Tayko</a>: Modern four-star hotel, £160, just inaugurated.\
                                  <br/> <span>&#8226;</span> <a href=\"https://www.marriott.co.uk/hotels/travel/svqlc-hotel-alfonso-xiii-a-luxury-collection-hotel-seville/\">Hotel Alfonso XIII</a>: stunning five-star hotel, but rooms are £300-1000 per night!\
-                                 <br />You can book them directly through their websites, or on <a href=\"https://www.booking.com\">Booking.com</a> or <a href=\"https://www.airbnb.com\">airbnb.com</a> . </p>\
+                                 <br />You can book them directly through their websites, on <a href=\"https://www.booking.com\">Booking.com</a> or <a href=\"https://www.airbnb.com\">airbnb.com</a> . </p>\
                                  <h4 align=\"left\" style=\"padding-top:10px;\">Travel Documents <span style=\"color:red\">(important)</span></h4>\
                                  <p align=\"left\">You will need three documents to enter Spain:<br/><span>&#8226;</span>From November 2023, non-EU nationals will need to get an <a href=\"https://www.spainvisa.eu/visa-types/etias/\">ETIAS permit</a> before travelling to Spain. The process is done online, takes around 5 minutes and costs €7 per person. The permit is valid for 3 years and multiple trips to Spain and EU countries of up to 90 days per 180-day period. <br/><span>&#8226;</span>A passport, which needs to be valid for a minimum of 3 months from the day of entry to Spain. <br/><span>&#8226;</span>Your boarding pass for the flight.<br/><br/>You need to take all three documents with you when you fly to Spain (on paper or digitally).</p>",
       onTheDaySectionTitle: "On The Day",
       onTheDaySectionDescription: "<p align=\"left\"><strong>Please make sure that you arrive at the venue between TBD and TBD.</strong></p> \
-      <h4 align=\"left\" style=\"padding-top:10px;\">Itinerary</h4>\
-      <p align=\"left\">12:00-13:00 <strong>Wedding Photos</strong><br/>The grooms and the most immediate family members will take wedding photos at the Plaza de España.<br/>13:00-13:45 <strong>Wedding Ceremony</strong><br/>The ceremony will be outdoors at the Alfonso XIII.<br/>14:00-15:00 <strong>Reception</strong><br/>Stand-up social function where beverages and light foods are served. Foods may be presented on small buffet tables or passed by servers.<br/>15:15-17:30 <strong>Banquet</strong><br/><br/>18:00-22:00 <strong>Dance Floor</strong>The hall will be cleared and converted into a dance floor. All drinks will continue to be free and unlimited.<br/>22:00 Onwards</br>The official event ends here, but there will be some groups of guests who decide to go to nearby bars or clubs.</p> \
+      <h4 align=\"left\" style=\"padding-top:10px;\">Itinerary (to be confirmed)</h4>\
+      <p align=\"left\">12:00-13:00 <strong>Wedding Photos</strong><br/>The grooms and the most immediate family members will be taking wedding photos before going to the venue.<br/>13:00-13:45 <strong>Reception</strong><br/>We will serve drinks (not alcoholic just yet!) and a big selection of appetisers. Both the Reception and the Ceremony will be outdoors. The ceremony will be outdoors. We will make sure that there is enough shade, fans or ACs if the day is hot. <br/>14:00-15:00 <strong>Wedding Ceremony</strong><br/>By the fountain. <br/>15:15-17:30 <strong>Wedding Breakfast</strong><br/>We will have a sit-down meal with a starter, a main, a dessert and unlimited drinks, including alcohol. We will aim to do it outside, but we may do it indoors if the day is too hot.<br/>18:00-00:00 <strong>Reception</strong><br/>We will set up a dance floor at the Columns Patio, next to the open bar and a chillout room. All drinks will continue to be free and unlimited. You will also be able to go out to the garden if you need some air.<br/>00:00 <strong>Onwards</strong></br>The official event ends here. Some guests may decide to go to nearby bars or clubs.</p> \
       <h4 align=\"left\" style=\"padding-top:10px;\">Drinks</h4>\
-      <p align=\"left\">We will cover unlimited drinks, including alcoholic, for the duration of the wedding (around 7 hours). The drinks that will be served during the Reception and the Banquet are: beer, soft drinks, water, white wine, red wine and cava (Spanish sparkling wine).<br/><br/>During the Dance Floor time, there will be different drinks available:<br/>- Beer, juices, soft drinks and cava.<br/>- Whisky: JB, Ballantine’s and White Label.<br/>- Vodka: Absolut and Eristoff.<br/>- Gin: Beefeater, Tanqueray and Puerto de Indias.<br/>- Rum: Bacardi 5 years, Cacique, Brugal and Barceló. <br/>- Liqueurs: Cointreau, Baileys, Frangélico, Pacharán, Brandy, Alcohol-free apple and Alcohol-free peach.</p> \
+      <p align=\"left\">We will cover unlimited drinks, including alcoholic, during the Breakfast and the Dance (around 8 hours in total). During the meal, they will serve the following drinks: TBD.<br/><br/>During the Reception, there will be different drinks available (TBC):  White Martini, White and Añejo Rum, Pacharán, Strawberry Gin SK, Vodka, Tequila, Creams, alcoholic and non-alcoholic liquors, beer, soft drinks, juices.</p> \
       <h4 align=\"left\" style=\"padding-top:10px;\">Food</h4>\
-      <p align=\"left\"><span>&#8226;</span> At the Reception, we will have various nibbles.\
-      <br/><span>&#8226;</span> At the Banquet, the menu will consist of:</p>\
+      <p align=\"left\"><span>&#8226;</span> At the Cocktail, we will have various nibbles.\
+      <br/><span>&#8226;</span> At the Breakfast, the menu will consist of:</p>\
       <div style=\"margin-left: 20px; text-align: left;\">-Starter: TBD\
       <br/>-Main: TBD\
       <br/>-Dessert: TBD\
       <br/>If you have any allergies or can't eat any of the ingredients (nuts, gluten, meat, fish, pork, dairy), please let us know and we will try to accommodate your request.</div>\
-      <p align=\"left\"><span>&#8226;</span> A couple of hours after the Banquet, there will be an additional round of nibbles.</p> \
+      <p align=\"left\"><span>&#8226;</span> A few hours after the Breakfast, there will be an additional round of nibbles.</p> \
       <h4 align=\"left\" style=\"padding-top:10px;\">Etiquette</h4>\
-      <p align=\"left\">Most women will wear a dress, short or long, and men a suit or just a blazer, with or without tie. If you decide to wear a long dress, be aware that the floor outside the venue is similar to compressed sand and could easily stain long dresses. If you want a male or female style reference, you can have a look The dress code for our wedding is <a href=\"https://www.brides.com/semi-formal-wedding-attire-4800687\">at a \"semi-formal/cocktail attire\"</a>.</p> \
+      <p align=\"left\">Most women will wear a dress, short or long, and men a suit or just a blazer, with or without tie. If you decide to wear a long dress, be aware that the floor outside the venue is similar to compressed sand and could easily stain long dresses. If you want a style reference, you can have a look at a <a href=\"https://www.brides.com/semi-formal-wedding-attire-4800687\">semi-formal/cocktail attire</a>.</p> \
       <h4 align=\"left\" style=\"padding-top:10px;\">Wedding Gifts</h4>\
       <p align=\"left\">Your presence at the wedding is already a gift to us, and we understand that your international travel can be costly. We therefore do not expect a wedding gift from every group of guests.\
       <br/>If you do want to make a wedding gift, we would ask that you avoid physical gifts if possible, due to our international situation. However, we want to raise funds towards the process for us to have children, and we would be delighted if you would help us financially to achieve that goal.\
@@ -60,7 +60,7 @@ const translations = {
       <br/>Beneficiary: Lucas Rodriguez Diaz</p>",
       rsvpTitle: "RSVP",
       rsvpDescription: "<p id=\"rsvpDescription\">If you haven't already, please submit your RSVP by DATE TBD. We will only be able to accommodate the people explicitly listed below, due to budget constraints. If you think we have missed someone, please do get in touch.</p>",
-      preWeddingSectionTitle: "Pre-wedding Days",
+      preWeddingSectionTitle: "Pre-Wedding Days",
       preWeddingSectionDescription: "<p align=\"left\">We will do three activities before the wedding:\
       <h4 align=\"left\" style=\"padding-top:10px;\">UK Stag Do</h4>\
       <p align=\"left\">To be announced.</p>\
@@ -108,7 +108,7 @@ const translations = {
       theWeddingTitle: "La Boda",
       theWeddingLocation: "En Sevilla, España",
       theWeddingVenue: "En Villa Luisa.",
-      theWeddingDate: "Domingo, May 19th, 2024",
+      theWeddingDate: "Domingo, 2 de Junio de 2024",
       theWeddingDateComment: "La fecha ya está CONFIRMADA.",
       travelSectionTitle: "Viaje",
       rsvpTitle: "Confirmación de Asistencia",
